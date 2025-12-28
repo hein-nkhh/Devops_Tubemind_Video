4509d24a82ae8b8ec47e837a5108a17bc8f5c24a
Nguyễn Thị Thanh Tuyền <22521632@gm.uit.edu.vn>
2025-12-28 23:31:19 +0700

    Add notifier

---
+++ services/notifier-service/src/main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import time
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
+
+from core import process_message
+from libs.common.redis_client import redis_client
+from libs.common.constants import QUEUE_NOTIFIER
+
+
+def run_notifier():
+    print("Notifier service started...")
+
+    while True:
+        try:
+            message = redis_client.wait_for_task(QUEUE_NOTIFIER, timeout=5)
+
+            if message:
+                process_message(message)
+            else:
+                time.sleep(1)
+
+        except Exception as e:
+            print("[Notifier] Worker error:", e)
+            time.sleep(3)
+
+
+if __name__ == "__main__":
+    run_notifier()
